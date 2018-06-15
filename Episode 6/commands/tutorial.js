0b5eb1436eac5e4fff18290ae980784ccadfe315
Spimy <34989646+Spimy@users.noreply.github.com>
2018-06-15 20:06:54 +0400

    Add files via upload

---
+++ Episode 6/commands/tutorial.js
@@ -0,0 +1,11 @@
+const discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+  return message.channel.send('Hello World!');
+
+};
+
+module.exports.help = {
+  name: "tutorial"
+};
